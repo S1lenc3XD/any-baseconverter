5dbb3d0bafbe9ac867bd4bc81d7472357b8ab79c
S1lenc3XD <144755002+S1lenc3XD@users.noreply.github.com>
2023-11-02 01:35:35 +0000

    Put code in converter.py

--- converter.py
+++ converter.py
@@ -1,1 +1,25 @@
-
+while True:
+    while True:
+        try:
+            frombase = input("What base number would you like to like to convert from? ")
+            frombase = int(frombase)
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    while True:
+        try:
+            tobase = input("What base number would you like to convert to? ")
+            tobase = int(tobase)
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    while True:
+        try:
+            fromnum = input("Enter your base " + str(frombase) + " number that you want to convert to base " + str(tobase) + ": ")
+            fromnum = int(fromnum, frombase)
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid base " + str(frombase) + " number.")
+    
